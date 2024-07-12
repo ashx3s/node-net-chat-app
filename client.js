cdbdab0c32f4d6fcc66d8d12d2483a990be4b4e4
Ashlyn Knox <ashlyn@lilyx.io>
2024-07-12 13:42:12 -0600

    all users get messages

--- client.js
+++ client.js
@@ -7,21 +7,25 @@ const rl = readLine.createInterface({
   output: process.stdout,
 });
 
-const client = net.createConnection(
+const socket = net.createConnection(
   { host: "127.0.0.1", port: 3000 },
   async () => {
     console.log("connected to server");
     const message = await rl.question("Enter a message: ");
-    client.write(message);
+    socket.write(message);
   }
 );
 
+// receiving back from server and then logging it
+socket.on("data", (data) => {
+  console.log(data.toString("utf-8"));
+});
 // fires when the stream is closed
-client.on("close", () => {
+socket.on("close", () => {
   console.log("close");
 });
 
 // this fires before close
-client.on("end", () => {
+socket.on("end", () => {
   console.log("connection was ended");
 });
--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const net = require("net");
 // creates the server that can be reached by clients
 const server = net.createServer();
 
+// save all sockets in this list of clients
+const clients = [];
+
 server.on("connection", (socket) => {
   console.log("new connection to server");
+
   socket.on("data", (data) => {
-    console.log(data.toString("utf-8"));
+    clients.map((s) => {
+      s.write(data);
+    });
   });
+  clients.push(socket);
 });
 
 server.listen(3000, "127.0.0.1", () => {
