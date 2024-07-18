1ef5875af9b081415474a5736bbcfaf1e9c1b654
Ashlyn Knox <ashlyn@lilyx.io>
2024-07-18 16:21:19 -0600

    tty ux improvements clear line and move cursor

--- client.js
+++ client.js
@@ -7,25 +7,59 @@ const rl = readLine.createInterface({
   output: process.stdout,
 });
 
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, (error) => {
+      if (error) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, (error) => {
+      if (error) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
 const socket = net.createConnection(
   { host: "127.0.0.1", port: 3000 },
   async () => {
     console.log("connected to server");
-    const message = await rl.question("Enter a message: ");
-    socket.write(message);
+
+    const ask = async () => {
+      const message = await rl.question("Enter a message: ");
+      // only need vertical because this is TTY. -1 is 1 line up
+      await moveCursor(0, -1).catch((error) =>
+        console.error("error moving cursor", error)
+      );
+      // clear current line that cursor is in
+      await clearLine(0).catch((error) => {
+        console.error("error clearing line", error);
+      });
+      socket.write(message);
+    };
+    ask();
+
+    // receiving back from server and then logging it
+    socket.on("data", async (data) => {
+      await moveCursor(0, -1);
+      await clearLine(0);
+      console.log(data.toString("utf-8"));
+      ask();
+    });
   }
 );
 
-// receiving back from server and then logging it
-socket.on("data", (data) => {
-  console.log(data.toString("utf-8"));
-});
-// fires when the stream is closed
-socket.on("close", () => {
-  console.log("close");
-});
-
-// this fires before close
 socket.on("end", () => {
   console.log("connection was ended");
 });
