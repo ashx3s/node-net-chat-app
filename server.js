20eb5e69ba5aa15a2a178e1a0dc78ca140c79d88
Ashlyn Knox <ashlyn@lilyx.io>
2024-07-19 15:52:35 -0600

    added id support + better data handling

--- server.js
+++ server.js
@@ -10,12 +10,19 @@ const clients = [];
 server.on("connection", (socket) => {
   console.log("new connection to server");
 
+  const clientId = clients.length + 1;
+  socket.write(`id-${clientId}`);
+
   socket.on("data", (data) => {
-    clients.map((s) => {
-      s.write(data);
+    const dataString = data.toString("utf-8");
+    const id = dataString.substring(0, dataString.indexOf("-"));
+    const message = dataString.substring(dataString.indexOf("-message-") + 9);
+    clients.map((client) => {
+      client.socket.write(`> User ${id}: ${message}`);
     });
   });
-  clients.push(socket);
+
+  clients.push({ id: clientId.toString(), socket });
 });
 
 server.listen(3000, "127.0.0.1", () => {
